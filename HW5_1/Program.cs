7e0231dd98ccab86bd6edb79cfd68263d2aa5594
elena tarasova <nafanichka.ev@gmail.com>
2022-10-02 20:40:51 -0400

    First exersize

---
+++ HW5_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] array = GetArray(10, 100, 1000);
+Console.WriteLine(String.Join(" ", array));
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return res;
+}
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (i%2==0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"ammount of even numbers is {count}");
